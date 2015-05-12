112b1952c486180e7eb7a4411bdd39089d19f19d
rng1234 <imryan.ng@gmail.com>
2015-05-12 17:29:00 +0800

    update ex22

---
+++ ex19.py
@@ -0,0 +1,13 @@
+def alex(age, height):
+	print "alex is %d old, %d hiehgt." % (age, height)
+	answer = raw_input("do you like him?")
+	print "Your answer is %s." % answer
+
+	
+print "let's do this."
+ask1 = int(raw_input("How old are you?"))
+ask2 = int(raw_input("How tall are you?"))
+
+
+
+alex(ask1, ask2)
